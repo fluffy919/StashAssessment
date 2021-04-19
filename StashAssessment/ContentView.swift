ccf861a1907d1d4a9d60b2054cf73b71270d9871
mobileANDweb <mobiledeveloper112075@gmail.com>
2021-04-19 14:49:05 -0400

    Initial Commit

---
+++ StashAssessment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StashAssessment
+//
+//  Created by B on 4/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StashAssessment/StashAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  StashAssessmentApp.swift
+//  StashAssessment
+//
+//  Created by B on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct StashAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
